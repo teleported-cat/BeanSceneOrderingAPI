5dbb4675f91c24e85638bfa38e8585ab112d3567
Pat <patrickross216@gmail.com>
2025-09-11 21:46:38 +1000

    Creates items controller

---
+++ BeanSceneOrderingAPI/Controllers/ItemsController.cs
@@ -0,0 +1,28 @@
+﻿using BeanSceneOrderingAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace BeanSceneOrderingAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        MongoClient client;
+        string databaseName;
+
+        public ItemsController(IOptions<BeanSceneDatabaseSettings> databaseSettings)
+        {
+            databaseName = databaseSettings.Value.DatabaseName;
+            client = new MongoClient(databaseSettings.Value.ConnectionString);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var collection = client.GetDatabase(databaseName).GetCollection<Item>("Items").AsQueryable();
+            return collection == null ? NotFound() : Ok(collection);
+        }
+    }
+}
