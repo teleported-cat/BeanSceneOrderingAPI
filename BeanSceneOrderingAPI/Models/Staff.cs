cbf4791c2ae18d99c1af8c69cb4822c7cd9200c8
Pat <patrickross216@gmail.com>
2025-10-31 15:48:39 +1100

    Creates password-less DTO class of staff

--- BeanSceneOrderingAPI/Models/Staff.cs
+++ BeanSceneOrderingAPI/Models/Staff.cs
@@ -22,4 +22,21 @@ namespace BeanSceneOrderingAPI.Models
         [BsonElement("role")]
         public string Role { get; set; }
     }
+    public class StaffDto
+    {
+        [BsonId] // Primary Key
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)] // Pass the object id as string
+        [JsonPropertyName("_id")]
+        public string? Id { get; set; }
+        [BsonElement("firstname")]
+        public string FirstName { get; set; }
+        [BsonElement("lastname")]
+        public string LastName { get; set; }
+        [BsonElement("username")]
+        public string Username { get; set; }
+        [BsonElement("email")]
+        public string Email { get; set; }
+        [BsonElement("role")]
+        public string Role { get; set; }
+    }
 }
